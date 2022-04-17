08a4aeeda6bc5d0ef733da306bb30ce30cf16ec5
Deepakumar <gdeepakumar@gmail.com>
2022-04-17 15:47:51 +0530

    GA3

--- src/pages/home/main.js
+++ src/pages/home/main.js
@@ -5,12 +5,19 @@ import router from './router'
 import ECharts from 'vue-echarts'
 import "echarts";
 import Vuelidate from 'vuelidate'
+import VueAnalytics from 'vue-analytics';
 // import 'materialize-css/dist/css/materialize.min.css'
 // import 'material-design-icons/iconfont/material-icons.css'
 
 Vue.component('v-chart', ECharts)
 
 Vue.use(Vuelidate)
+
+Vue.use(VueAnalytics, {
+  id: 'G-37PVYZFCHN',
+  router
+});
+
 Vue.config.productionTip = false
 new Vue({
   vuetify,
